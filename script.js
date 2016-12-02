21e6429988600096bc3d20c0494a8f047062ee1b
sophiayangx <sopyang@nuevaschool.org>
2016-12-02 08:57:45 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var editor = $(parent.document.body)
 editor.find("div").css({background:"black"})
 
 var topbar = $(parent.parent.document.body).find("#topbar")
-//topbar.find("div").css({border: "2px solid black})
-topbar.find("div").append("<div>" + "hi" + "</div>")
+topbar.find("div").css({border: "2px solid yellow"});
+topbar.find("div").append("hellow")
 
 var allDivsInTheEditor = editor.find("div")
 
