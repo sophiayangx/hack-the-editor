a9cb2c1d5e5fb098c818dec271069166401d8f9f
sophiayangx <sopyang@nuevaschool.org>
2016-12-02 08:41:40 -0800

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,14 @@
+var editor = $(parent.document.body)
+editor.find("div").css({background:"yellow"})
+
+var topbar = $(parent.parent.document.body).find("#topbar")
+
+var allDivsInTheEditor = editor.find("div")
+
+
+
+// Now try to
+// 1. add a button to the topbar
+// 2. change the title above the file list
+// 3. change some borders in the editor
+// 4. Make all this happen when a button is clicked
