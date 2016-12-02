0657d69b0ff88a355fee39231a9aac2906415de8
sophiayangx <sopyang@nuevaschool.org>
2016-12-02 09:08:53 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-button.click(function(){
+var button = $("button")
 
+button.click(function(){
+  
 var editor = $(parent.document.body)
-//editor.find("div").css({border:"1px solid yellow"})
+editor.find("div").css({border:"1px solid yellow"})
 
 var topbar = $(parent.parent.document.body).find("#topbar")
 topbar.find("div").append("<button class='btn btn-primary'>" + "hi" + "</button>");
-//topbar.find("div").append("hellow")
 
 var allDivsInTheEditor = editor.find("div")
 var projectheader = $(parent.document.body).find("#project-files-header")
